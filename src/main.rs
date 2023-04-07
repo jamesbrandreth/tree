2043224c876c3265d3fbccf96bd4f3bfc7a686ec
Nathan Samra <me@nathansamra.com>
2023-04-07 17:01:42 +0100

    Make Node contain its Rc<RefCell>>
    The primary motivation for this was
    so add_child could be responsible for
    setting the child's parent. Parent
    should only ever be set by calling
    add_child, thereby avoiding parent
    and child becoming out of sync.
    
    The extra benefit of this change is
    Rc<RefCell<NodeData>> is now an
    implementation detail of Node, so
    Rc and RefCell don't need to be
    used in the main function
    at all.
    
    NodeData is a class that should
    always remain private to the node
    module.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,37 @@
 mod node;
 use node::Node;
 
-use std::cell::RefCell;
-use std::rc::Rc;
-
-fn add_child(current_node: Rc<RefCell<Node>>, new_node: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
-    current_node.borrow_mut().add_child(new_node.clone());
-    new_node
-}
-
 fn main() {
     // Make a root node
-    let root = Rc::new(RefCell::new(Node::new("Hi, i'm root!", None)));
+    let root = Node::new("Hi, i'm root!");
     // Set the "current" node to the root
     let mut current_node = root.clone();
 
     // Make a child and add it to the root, update the current node to be the child
-    let child = Rc::new(RefCell::new(Node::new(
-        "Hi, i'm a child!",
-        Some(root.clone()),
-    )));
-    current_node = add_child(current_node, child);
+    let child = Node::new("Hi, i'm a child!");
+    current_node.add_child(child.clone());
+    current_node = child;
 
-    let grandchild = Rc::new(RefCell::new(Node::new(
-        "Hi, i'm a grandchild!",
-        Some(current_node.clone()),
-    )));
-    current_node = add_child(current_node, grandchild);
+    let grandchild = Node::new("Hi, i'm a grandchild!");
+    current_node.add_child(grandchild.clone());
+    current_node = grandchild;
 
     // Move up a node
-    let next_node = current_node.borrow().parent().as_ref().unwrap().clone();
+    let next_node = current_node.parent().as_ref().unwrap().clone();
     current_node = next_node;
 
-    let grandchild = Rc::new(RefCell::new(Node::new(
-        "Hi, i'm another grandchild!",
-        Some(current_node.clone()),
-    )));
-    current_node = add_child(current_node, grandchild);
+    let grandchild = Node::new("Hi, i'm another grandchild!");
+    current_node.add_child(grandchild.clone());
+    current_node = grandchild;
 
     // Move up two nodes
-    let next_node = current_node.borrow().parent().as_ref().unwrap().clone();
+    let next_node = current_node.parent().as_ref().unwrap().clone();
     current_node = next_node;
-    let next_node = current_node.borrow().parent().as_ref().unwrap().clone();
+    let next_node = current_node.parent().as_ref().unwrap().clone();
     current_node = next_node;
 
-    let grandchild = Rc::new(RefCell::new(Node::new(
-        "Hi, i'm a child!",
-        Some(current_node.clone()),
-    )));
-    current_node = add_child(current_node, grandchild);
+    let grandchild = Node::new("Hi, i'm a child!");
+    current_node.add_child(grandchild);
 
-    println!("{}", root.borrow());
+    println!("{}", root);
 }
--- src/node.rs
+++ src/node.rs
@@ -1,43 +1,55 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub type SharedNode = Rc<RefCell<Node>>;
-
-pub struct Node {
+struct NodeData {
     data: &'static str,
-    parent: Option<SharedNode>,
-    children: Vec<SharedNode>,
+    parent: Option<Node>,
+    children: Vec<Node>,
+}
+
+#[derive(Clone)]
+pub struct Node {
+    data: Rc<RefCell<NodeData>>,
 }
 
 impl Node {
-    pub fn new(data: &'static str, parent: Option<SharedNode>) -> Self {
-        Self {
+    pub fn new(data: &'static str) -> Self {
+        let node_data = Rc::new(RefCell::new(NodeData {
             data,
-            parent,
+            parent: None,
             children: vec![],
-        }
+        }));
+
+        Self { data: node_data }
     }
 
-    pub fn parent(&self) -> &Option<SharedNode> {
-        &self.parent
+    pub fn parent(&self) -> Option<Node> {
+        self.data.borrow().parent.clone()
     }
 
-    pub fn add_child(&mut self, child: SharedNode) {
-        self.children.push(child);
+    fn set_parent(&mut self, parent: Node) {
+        self.data.borrow_mut().parent = Some(parent);
+    }
+
+    pub fn add_child(&mut self, mut child: Node) {
+        child.set_parent(self.clone());
+        self.data.borrow_mut().children.push(child);
     }
 }
 
 impl std::fmt::Display for Node {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let mut children: String = self
+        let node_data = self.data.borrow();
+
+        let mut children: String = node_data
             .children
             .iter()
-            .map(|child| format!("{}\n", child.borrow()))
+            .map(|child| format!("{}\n", child))
             .collect();
         children = children
             .lines()
             .map(|line| format!("\n  {}", line))
             .collect();
-        write!(f, "{}{}", self.data, format!("{}", children))
+        write!(f, "{}{}", node_data.data, format!("{}", children))
     }
 }
